f78e8bb2aaaff80af53cb117370ef8358b798fb1
SuperKlyde <hugo.capes@epita.fr>
2016-12-26 16:06:37 +0100

    [INIT] First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+test_nlpf
+=========
+
+A Symfony project created on December 18, 2016, 6:15 pm.
---
+++ tests/firstTest.php
@@ -0,0 +1,9 @@
+<?php
+class TravisTest extends PHPUnit_Framework_TestCase
+{
+    public function testSimple()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
+?>
