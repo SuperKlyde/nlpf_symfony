cfadc8cc964191ae94fa6a0b5ab256a7ee5760ec
Simon MACE <simon.mace@epita.fr>
2017-01-06 11:55:15 +0100

    [DB] Added shell command to create DB

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -17,5 +21,14 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Get("/createdb")
+     */
+    public function createDb(Request $request) {
+        shell_exec("php bin/console doctrine:database:create");
+        shell_exec("php bin/console doctrine:schema:update --dump-sql --force");
+        return new JsonResponse("Created DB");
     }
 }
