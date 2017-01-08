d4ab5fd8d0a40ff721244c3394eaca3732d537c5
Simon MACE <simon.mace@epita.fr>
2017-01-08 17:08:17 +0100

    [VIEWS] We now have accueil page and create account

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,4 +22,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/createaccount", name="createaccount")
+     */
+    public function createAccountAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/createaccount.html.twig', [
+          'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
